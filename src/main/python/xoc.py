2d12a7c61f5e3308ccd8f62747f7c241707c8711
eliquinox <eliquinox@outlook.com>
2023-02-21 21:05:03 +0000

    xoc.py

---
+++ src/main/python/xoc.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import serial
+import sys
+
+port = "/dev/ttyUSB1"
+
+
+def number_to_ascii(n):
+    return bytes(chr(n), encoding='ascii')
+
+
+def bid(price, size, s):
+    s.write(number_to_ascii(0))
+    s.write(number_to_ascii(price))
+    s.write(number_to_ascii(size))
+
+
+def ask(price, size, s):
+    s.write(number_to_ascii(1))
+    s.write(number_to_ascii(price))
+    s.write(number_to_ascii(size))
+
+
+def transmit_book_data(s):
+    ret = s.readline()[-4:]
+    if ret:
+        bid_price = ret[0]
+        bid_size = ret[1]
+        ask_price = ret[2]
+        ask_size = ret[3]
+        print(f"Bid Price: {bid_price} | Bid Size: {bid_size} | Ask Price: {ask_price} | Ask Size: {ask_size}")
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    side = args[1]
+    price = int(args[2])
+    size = int(args[3])
+
+    with serial.Serial(port, timeout=2, baudrate=9600) as ser:
+        if side == 'bid':
+            bid(price, size, ser)
+
+        elif side == 'ask':
+            ask(price, size, ser)
+
+        else:
+            raise RuntimeError(f"unknown side {side} provided")
+
+        transmit_book_data(ser)
